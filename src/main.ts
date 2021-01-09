8eda5ddd979744fda1b09df52e98bb0248c0f463
rafa11rodrigues <rafael11rodrigues@hotmail.com>
2021-01-09 16:59:31 -0300

    added vue material

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './styles.scss';
+import './theme.scss';
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
+
 Vue.config.productionTip = false
 
 new Vue({
